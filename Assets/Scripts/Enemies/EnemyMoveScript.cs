cd270608966448a9a58021d5bfa9eb0f3b013596
TitoVice <50460208+TitoVice@users.noreply.github.com>
2021-04-20 01:45:32 +0200

    escena de practiques posat

---
+++ Assets/Scripts/Enemies/EnemyMoveScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMoveScript : MonoBehaviour
+{
+    private Transform target;
+    private NavMeshAgent agent;
+    public string characterName;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    void Update()
+    {
+        if(GetComponent<NavMeshAgent>().enabled == true)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    public void Immobilize()
+    {
+        GetComponent<NavMeshAgent>().enabled = false;
+    }
+
+    public void Mobilize()
+    {
+        GetComponent<NavMeshAgent>().enabled = true;
+    }
+}
